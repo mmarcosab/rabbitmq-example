4b339be5f5649b0fe4b2e0bb0a00f469505cbbcf
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-10-01 22:20:03 -0300

    poc com rabbit

--- src/main/java/com/example/rabbitmq/app/AppApplication.java
+++ src/main/java/com/example/rabbitmq/app/AppApplication.java
@@ -1,14 +1,20 @@
 package com.example.rabbitmq.app;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class AppApplication {
 
+	@Autowired
+	static OrderQueueSender orderQueueSender;
+
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+		orderQueueSender.send("teste do proprio programa");
 	}
 
 }
