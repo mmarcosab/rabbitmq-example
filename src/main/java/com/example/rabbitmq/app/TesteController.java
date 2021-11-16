acad57737405db8809f4e90a60aa5ba32a0e4d27
Marcos Vinicius <marcosab.vinicius@gmail.com>
2021-11-15 23:24:00 -0300

    add header

--- src/main/java/com/example/rabbitmq/app/TesteController.java
+++ src/main/java/com/example/rabbitmq/app/TesteController.java
@@ -1,7 +1,8 @@
 package com.example.rabbitmq.app;
 
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/teste")
 public class TesteController {
 
-    @Autowired
-    private AmqpTemplate queueSender;
+    public TesteController(AmqpTemplate queueSender) {
+        this.queueSender = queueSender;
+    }
+
+    private final AmqpTemplate queueSender;
 
     @GetMapping
     public String send(){
 
-        queueSender.convertAndSend("teste-exchange", "routing-key-teste", "test message");
+        String mensagem = "test message";
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setHeader("ultima", "sim");
+        Message message = new Message(mensagem.getBytes(), messageProperties);
+
+        queueSender.convertAndSend("teste-exchange", "routing-key-teste", message);
         return "ok. done";
     }
 
